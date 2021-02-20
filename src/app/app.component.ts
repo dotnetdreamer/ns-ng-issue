44f3eac2fbb44ae81522d14298962fd134943adf
Idrees Khan <dotnetdreamer@users.noreply.github.com>
2021-02-20 11:00:41 +0400

    fgh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,39 @@
 import { OnInit } from "@angular/core";
+import { NgZone } from "@angular/core";
 import { Component } from "@angular/core";
 import { RouterExtensions } from "@nativescript/angular";
+import { NgxPubSubService } from "@pscoped/ngx-pub-sub";
 
 @Component({
     selector: "ns-app",
     templateUrl: "./app.component.html"
 })
 export class AppComponent implements OnInit { 
-    constructor(private routerExt: RouterExtensions) {
-
+    constructor(private routerExt: RouterExtensions
+        , private ngZone: NgZone
+        , private pubsubSvc: NgxPubSubService) {
+        this._init();
     }
 
     async ngOnInit() {
-        const numOfVehicles = await this._testPromise();
-        if(numOfVehicles == 0) {
-            await this.routerExt.navigate(['/home']);
-        } else {
-            await this.routerExt.navigate(['/parking']);
-        }
+        this.pubsubSvc.publishEvent('init');
+
     }
 
+    private _init() {
+        this.pubsubSvc.subscribe('init', async () => {
+            const numOfVehicles = await this._testPromise();
+            if(numOfVehicles == 0) {
+                this.ngZone.run(async () => {
+                    await this.routerExt.navigate(['/home']);
+                });
+            } else {
+                this.ngZone.run(async () => {
+                    await this.routerExt.navigate(['/parking']);
+                }); 
+            }
+        });
+    }
     
     private _testPromise() {
         return new Promise((resolve, reject) => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "@nativescript/angular";
 
+import { NgxPubSubModule } from "@pscoped/ngx-pub-sub";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ItemsComponent } from "./item/items.component";
@@ -11,7 +13,8 @@ import { ItemsComponent } from "./item/items.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NgxPubSubModule
     ],
     declarations: [
         AppComponent,
